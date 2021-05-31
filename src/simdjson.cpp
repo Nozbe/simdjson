84f3a9ccb9517c0be816892896612a780f7c1b87
radex <radexpl@gmail.com>
2021-05-31 10:49:10 +0200

    Updating src files for simdjson v0.9.5

--- src/simdjson.cpp
+++ src/simdjson.cpp
@@ -1,4 +1,4 @@
-/* auto-generated on 2021-05-27 16:20:07 -0400. Do not edit!x */
+/* auto-generated on 2021-05-27 16:20:07 -0400. Do not edit! */
 /* begin file src/simdjson.cpp */
 #include "simdjson.h"
 
